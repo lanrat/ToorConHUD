91e938e2740fcb73d86be42f68ee68c12ae84d11
Ian Foster <mrlanrat@gmail.com>
2016-06-22 14:41:03 -0700

    page auto updates

---
+++ js/update.js
@@ -0,0 +1,16 @@
+// Check if a new cache is available on page load.
+window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+        // Browser downloaded a new app cache.
+        console.log("New Version, updating...");
+        window.location.reload();
+    } else {
+        // Manifest didn't changed. Nothing new to serve.
+    }
+}, false);
+function updateApp() {
+    console.log("Checking for app update");
+    window.applicationCache.update();
+}
+// check for new page version
+setInterval(updateApp,10*60*1000); // 10 min
