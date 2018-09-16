7b9cc4c7b4c4fcfa99d7833c1bbdf81e60e1faed
Ian Foster <mrlanrat@gmail.com>
2018-09-16 11:51:09 -0700

    changing refresh and bold time

--- js/update.js
+++ js/update.js
@@ -3,7 +3,8 @@ window.applicationCache.addEventListener('updateready', function(e) {
     if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
         // Browser downloaded a new app cache.
         console.log("New Version, updating...");
-        window.location.reload();
+        //window.location.reload();
+        window.location = window.location;
     } else {
         // Manifest didn't changed. Nothing new to serve.
     }
